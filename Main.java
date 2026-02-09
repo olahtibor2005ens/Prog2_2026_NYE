efd950fc532b95645178a88eba9b80a1f852ceed
Oláh Tibor <olah.tibor.2005@gmail.com>
2026-02-09 10:45:50 +0100

    1. Hét

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Oláh Tibor";
+        String neptunCode = "BHG9VH";
+        String favQuote = "If the world chooses to become my enemy, I will fight like I always have.";
+        System.out.printf("Hello %s (%s)!\n", name, neptunCode);
+        System.out.printf("Your favorite quote is: \"%s\"\n", favQuote);
+    }
+}
